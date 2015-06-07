befc590674775097e6c53dcb7ce6cc2286a7cbdb
Max Soe <maxsoe@gmail.com>
2015-06-07 22:16:05 +0100

    Italicise online course URLs

--- js/helper.js
+++ js/helper.js
@@ -54,7 +54,7 @@ var HTMLonlineClasses = '<h3>Online Classes</h3>';
 var HTMLonlineTitle = '<a href="#">%data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
+var HTMLonlineURL = '<br><a href="#" class="online-url">%data%</a>';
 
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
